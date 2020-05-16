1ecdcba947d8027965a452c35b346cd3ca633cf7
Pohaval <dminnikk@yandex.ru>
2020-05-16 11:23:13 +0700

    mod

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+	var owl = $('.slider-1').owlCarousel({
+		margin: 65,
+		nav: true,
+		autoWidth: 1,
+		navText: ["prev", "next"]//["<img src='img/arrow.png' width='15px'>", "<img src='img/arrow.png' width='15px'>"]
+	});
+
+});
