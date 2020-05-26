532253de859c8b251c99a7ecc70cbba11e2c0229
Pohaval <dminnikk@yandex.ru>
2020-05-26 23:24:13 +0700

    li

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
 
-	var owl = $('.slider-1').owlCarousel({
-		margin: 65,
+	const navslider = $('#navslider');
+	const bookslider = $('#bookslider');
+
+	navslider.owlCarousel({
+		margin: 20,
 		nav: true,
 		autoWidth: 1,
 		navText: ["prev", "next"]//["<img src='img/arrow.png' width='15px'>", "<img src='img/arrow.png' width='15px'>"]
 	});
+	bookslider.owlCarousel({
+		margin: 20,
+		items: 7,
+		nav: true,
+		loop: true,
+		smartspeed: 3000,
+		navText: ["prev", "next"]//["<img src='img/arrow.png' width='15px'>", "<img src='img/arrow.png' width='15px'>"]
+	});
+
 
 });
