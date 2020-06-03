66f3103033fffe142199434ca568e51ef3bbcb8d
Pohaval <dminnikk@yandex.ru>
2020-06-03 13:35:40 +0700

    all

--- js/script.js
+++ js/script.js
@@ -11,13 +11,14 @@ $(document).ready(function () {
 		navText: ["<img src='assets/img/icons/left-arrow.svg' width='15px'>", "<img src='assets/img/icons/right-arrow.svg' width='15px'>"]
 	});
 	bookslider.owlCarousel({
-		margin: 0,
+		autoWidth: '100%',
+		margin: 43,
 		items: 7,
 		nav: true,
 		loop: true,
 		dots: false,
 		smartspeed: 3000,
-		navText: ["<img src='assets/img/icons/left-arrow.svg' width='15px'>", "<img src='assets/img/icons/right-arrow.svg' width='15px'>"]
+		navText: ["<img src='assets/img/icons/left-arrow.svg' width='17px'>", "<img src='assets/img/icons/right-arrow.svg' width='17px'>"]
 	});
 
 
